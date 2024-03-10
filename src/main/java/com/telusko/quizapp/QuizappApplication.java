e1333c3c4f2be4d6aac195e825d89de1a6d0e1a4
Senthilkumar Muthusamy <notsen2000@gmail.com>
2024-03-09 21:20:36 -0600

    update0n09march20242120

--- src/main/java/com/telusko/quizapp/QuizappApplication.java
+++ src/main/java/com/telusko/quizapp/QuizappApplication.java
@@ -1,13 +1,19 @@
 package com.telusko.quizapp;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class QuizappApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(QuizappApplication.class, args);
+
+	 
 	}
 
 }
